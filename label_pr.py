da8338dd2965fb09f33c092b5f622ee8b62e3804
Isaac Owomugisha <archimedesisaac@gmail.com>
2023-06-12 10:38:34 +0300

    Fix issue where the bug-label already exists

--- label_pr.py
+++ label_pr.py
@@ -5,9 +5,9 @@ api = GhApi(owner='cmplx-xyttmt', repo='wandb-cicd')
 
 pr_number = os.getenv('PR_NUMBER', 1)
 
-api.issues.create_label(name='bug', color='ff0000', description='A label indicating a bug fix PR.')
+api.issues.create_label(name='custom-label', color='00ff00', description='A custom label, since the bug label already exists.')
 
-api.issues.add_labels(pr_number, labels=['bug'])
+api.issues.add_labels(pr_number, labels=['bug', 'custom-label'])
 
 comment = api.issues.create_comment('Labeled this PR as a bug fix.')
 
