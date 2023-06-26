a4043654301800d0115764f100c49cf5a72bbf20
Isaac Owomugisha <archimedesisaac@gmail.com>
2023-06-26 20:18:23 +0300

    Add project notebook. Run into W&B API error

--- params.py
+++ params.py
@@ -1,4 +1,4 @@
-WANDB_PROJECT = "mlops-course-001"
+WANDB_PROJECT = "mlops-course-001b"
 ENTITY = None  # set this to team name if working in a team
 BDD_CLASSES = {i:c for i, c in enumerate(['background', 'road', 'traffic light', 'traffic sign', 'person', 'vehicle', 'bicycle'])}
 RAW_DATA_AT = 'bdd_simple_1k'
